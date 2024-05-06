669583af38d1205ac1396e7067ddde0b6ff5abc0
xach55 <xach55@gmail.com>
2024-05-06 13:58:45 +0400

    initial commit

---
+++ SwiftUIFurnitureStore/SwiftUIFurnitureStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIFurnitureStore
+//
+//  Created by Levon Shaxbazyan on 06.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIFurnitureStore/SwiftUIFurnitureStore/SwiftUIFurnitureStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFurnitureStoreApp.swift
+//  SwiftUIFurnitureStore
+//
+//  Created by Levon Shaxbazyan on 06.05.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFurnitureStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
